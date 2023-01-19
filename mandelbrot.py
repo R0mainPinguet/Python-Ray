f543a2245ecf5fcac7e8f1dba4c6415a5a4b49d3
R0mainPinguet <98420700+R0mainPinguet@users.noreply.github.com>
2023-01-19 11:59:13 +0100

    brice's mandelbrot
    It works !!

--- mandelbrot.py
+++ mandelbrot.py
@@ -3,21 +3,20 @@ import matplotlib.pyplot as plt
 import ray
 import time
 
-ray.init()
+ray.init(num_cpus = 2)
 
 x_min = -1.78
 x_max = 0.78
 y_min = -0.961
 y_max = 0.961
 
-
 def Compute (im , width , height , nb_iter, x_min, x_max, y_min, y_max):
-      
+
     pos = 0
-          
+
     dx = (x_max - x_min) / width
     dy = (y_max - y_min) / height
-      
+
     for l in range(height):
         for c in range(width):
             a = x_min + c * dx
@@ -39,14 +38,10 @@ def Compute (im , width , height , nb_iter, x_min, x_max, y_min, y_max):
 
 @ray.remote
 def Compute_ray (im , width , height , nb_iter, x_min, x_max, y_min, y_max):
-
     im2 = im.copy()
-    
     pos = 0
-          
     dx = (x_max - x_min) / width
     dy = (y_max - y_min) / height
-      
     for l in range(height):
         for c in range(width):
             a = x_min + c * dx
@@ -62,39 +57,28 @@ def Compute_ray (im , width , height , nb_iter, x_min, x_max, y_min, y_max):
                     break
                 else:
                     i+=1
-
-            im2[pos//width][pos%width] =  i / nb_iter * 255
+            im2[pos//width][pos%width] = i/nb_iter*255
             pos += 1
+    return im2.copy()
 
-    return(im2.copy())
-  
-nb_iter = 20
-width = 512
-height = 420
+nb_iter = 500
+width = 2*512
+height = 2*420
 
-tStart = time.time()
-for i in range(5):
-    im = np.zeros((height,width),dtype='int')
-    Compute (im, width, height , nb_iter, x_min, x_max, y_min, y_max)
-
-    if(i==0):
-        plt.imsave("Serial.png",im/nb_iter)
-        
-tEnd = time.time()
-print("5 serial executions : " + str(tEnd - tStart) )
-
+#tStart = time.time()
+#im = np.zeros((height,width),dtype='int')
+#Compute(im, width, height , nb_iter, x_min, x_max, y_min, y_max)
+#tEnd = time.time()
+#plt.imsave("Serial.png", im/nb_iter)
+#print("Serial execution : " + str(tEnd - tStart))
 
 tStart = time.time()
-for i in range(5):
-    im = np.zeros((height,width),dtype='int')
-    im = Compute_ray.remote(im, width, height , nb_iter, x_min, x_max, y_min, y_max)
-    im = ray.get(im)
-    if(i==0):
-        plt.imsave("Ray.png",im/nb_iter)
-        
+im = np.zeros((height,width),dtype='int')
+results = ray.get([Compute_ray.remote(im[:420,:], width, 420, nb_iter, x_min, x_max, 0, y_max),Compute_ray.remote(im[420:,:], width, 420, nb_iter, x_min, x_max, y_min, 0)])
+im[:420,:] = results[0]
+im[420:,:] = results[1]
 tEnd = time.time()
-print("5 parallel executions : " + str(tEnd - tStart) )
-
-
+plt.imsave("Ray.png", im/nb_iter)
+print("Parallel execution : " + str(tEnd - tStart))
 
 ray.shutdown()
